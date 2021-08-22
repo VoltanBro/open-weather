95c40be5fe2d2d647fa6afde491f2732fb30114a
Vitalii Molchanov <etc.sharam@gmail.com>
2021-08-22 04:32:45 +0300

    Add styles for home page

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,25 @@
 module HomeHelper
+  def city_name
+    @body['name']
+  end
+
+  def temp
+    temp = @body.dig('main', 'temp').to_i
+    "#{temp.round(1)}C#{celsius_sym}"
+  end
+
+  def feels_like
+    temp = @body.dig('main', 'feels_like')
+    "#{temp.round(1)}C#{celsius_sym}"
+  end
+
+  def sky
+    @body['weather'].first['main'].downcase
+  end
+
+  private
+
+  def celsius_sym
+    "\xC2\xB0"
+  end
 end
